ba9d05547f09f413cf31f49a1475bf5a5b6f9620
shero121 <keithsherwin6764@gmail.com>
2023-11-22 10:20:20 +0000

    did part 3

--- finalStep.php
+++ finalStep.php
@@ -1,42 +1,38 @@
 <?php
 include("finalStepHeader.html");
 
-//translate the comments below into PHP code underneath each comment
+// Start a session
+session_start();
 
-//start a session
+// Echo the passenger's firstname from the appropriate session variable
+echo "First Name: " . $_SESSION['passengerFN'] . "<BR>";
 
-//echo the passenger's firstname from the appropriate session variable
+// Echo the passenger's surname from the appropriate session variable
+echo "Surname: " . $_SESSION['passengerSN'] . "<BR>";
 
-echo "<BR>";
+// If the luggage session variable is set
+if (isset($_SESSION['luggage'])) {
 
-//echo the passenger's surname from the appropriate session variable
+    // Echo the amount of bags under ten kilos the passenger is bringing
+    echo "Bags under 10 kg: " . $_SESSION['subTenKG'] . "<br>";
 
-echo "<BR>";
+    // Echo the amount of bags over ten kilos the passenger is bringing
+    echo "Bags over 10 kg: " . $_SESSION['overTenKG'] . "<br>";
+}
 
-// if the luggage session variable is on
-
-    //echo the amount of bags under ten kilos the passenger is bringing
-    
-    echo "<BR>";
-    
-    //echo the amount of bags over ten kilos the passenger is bringing
-    
-//end if block
-
+// End if block
 ?>
 </h4></div></div>
+
 <form method="POST" action="confirm.php">
     <div class="form-group">        
         <label class="control-label col-sm-12 text-center">Is the above information correct?</label>
         <div class="checkbox col-sm-12 text-center">
-          <label><input type="checkbox" name="confirm">Yes</input>
+          <label><input type="checkbox" name="confirm">Yes</label>
         </div>
     </div>
    
-        
-      <div class="col-sm-12 text-center">
+    <div class="col-sm-12 text-center">
         <button type="submit" class="btn btn-default">Continue</button>
-      </div>
     </div>
-    
 </form>
